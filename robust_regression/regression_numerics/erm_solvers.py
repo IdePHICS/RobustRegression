8f7b0526b86b8d17bcd337b8b84ba306ff61ec3b
Matteo Vilucchio <35531309+superporchetta@users.noreply.github.com>
2023-03-21 18:00:42 +0100

    Made Huber ERM work with Numba JIT

--- robust_regression/regression_numerics/erm_solvers.py
+++ robust_regression/regression_numerics/erm_solvers.py
@@ -1,12 +1,13 @@
-from numpy import divide, identity, sqrt, abs, count_nonzero, sum, dot, ones_like, inf, tile, finfo, float64
+from numpy import divide, identity, sqrt, abs, count_nonzero, sum, dot, ones_like, inf, tile, finfo, float64, empty, nonzero
+import numpy as np
+from numpy.linalg import norm
 from numpy.linalg import solve
 from numpy.random import normal
-from numba import njit
+from numba import njit, prange
 from scipy.optimize import minimize
 from cvxpy import Variable, Minimize, Problem, norm, sum_squares
-from sklearn.utils import axis0_safe_slice
-from sklearn.utils.extmath import safe_sparse_dot
-from regression_numerics import GTOL_MINIMIZE, MAX_ITER_MINIMIZE, BLEND_GAMP, TOL_GAMP
+from ..utils.matrix_utils import axis0_pos_neg_mask, safe_sparse_dot
+from ..regression_numerics import GTOL_MINIMIZE, MAX_ITER_MINIMIZE, BLEND_GAMP, TOL_GAMP
 
 
 @njit(error_model="numpy", fastmath=True)
@@ -28,7 +29,7 @@ def find_coefficients_L1(ys, xs, reg_param):
     return w.value
 
 
-@njit(error_model="numpy", fastmath=True)
+@njit(error_model="numpy")
 def _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a):
     linear_loss = ys - xs_norm @ w
     abs_linear_loss = abs(linear_loss)
@@ -44,14 +45,14 @@ def _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a):
     loss += 0.5 * dot(non_outliers, non_outliers)
     loss += 0.5 * reg_param * dot(w, w)
 
-    xs_non_outliers = -axis0_safe_slice(xs_norm, ~outliers_mask, n_non_outliers)
+    (xs_outliers, xs_non_outliers) = axis0_pos_neg_mask(xs_norm, outliers_mask, num_outliers)
+    xs_non_outliers *= -1.0
+
     gradient = safe_sparse_dot(non_outliers, xs_non_outliers)
 
     signed_outliers = ones_like(outliers)
     signed_outliers_mask = linear_loss[outliers_mask] < 0
     signed_outliers[signed_outliers_mask] = -1.0
-
-    xs_outliers = axis0_safe_slice(xs_norm, outliers_mask, num_outliers)
 
     gradient -= a * safe_sparse_dot(signed_outliers, xs_outliers)
     gradient += reg_param * w
@@ -84,3 +85,32 @@ def find_coefficients_Huber(ys, xs, reg_param, a):
         )
 
     return opt_res.x
+
+
+def find_coefficients_Huber_on_sphere(ys, xs, reg_param, q_fixed, a, gamma = 1e-04):
+    _, d = xs.shape
+    w = normal(loc=0.0, scale=1.0, size=(d,)) / sqrt(norm(w)) * sqrt(q_fixed)
+    xs_norm = divide(xs, sqrt(d))
+
+    loss, grad = _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a)
+    iter = 0
+    while iter < MAX_ITER_MINIMIZE and norm(grad) > GTOL_MINIMIZE:
+        if iter % 10 == 0:
+            print(str(iter) + 'th Iteration    Loss :: ' + str(loss) + ' gradient :: ' +  str(norm(grad)))
+
+        alpha = 1
+        new_w = w - alpha * grad
+        new_loss, new_grad = _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a)
+
+        while new_loss > loss - gamma * alpha * norm(grad):
+            alpha = alpha / 2
+            new_w = w - alpha * grad
+            new_loss, new_grad = _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a)
+
+        loss = new_loss
+        grad = new_grad
+        w = new_w
+
+        iter += 1
+
+    return w

f8cdad20185dd8ceedee81c342bc33b425cbf744
Matteo Vilucchio <35531309+superporchetta@users.noreply.github.com>
2023-03-23 09:40:09 +0100

    Fixed denoiser State Evolution and added example

--- robust_regression/fixed_point_equations/fpe_projection_denoising.py
+++ robust_regression/fixed_point_equations/fpe_projection_denoising.py
@@ -3,8 +3,7 @@ from math import sqrt
 
 # @njit(error_model="numpy", fastmath=True)
 def var_func_projection_denoising(m_hat, q_hat, sigma_hat, q_fixed):
-    eta_hat = m_hat **2 / q_hat
-    m = m_hat * sqrt(q_fixed) / sqrt(m_hat**2 + q_hat) # sqrt((q_hat + m_hat**2) * q_fixed / q_hat**3)  # (1 + eta_hat) * sqrt(q_fixed) * m_hat / q_hat
-    q = q_fixed # (1 + eta_hat) * q_fixed
-    sigma = sqrt(q_fixed * q_hat / (q_hat + m_hat**2))
+    m = m_hat * sqrt(q_fixed) / sqrt(m_hat**2 + q_hat)
+    q = q_fixed
+    sigma = sqrt(q_fixed / (q_hat + m_hat**2))
     return m, q, sigma

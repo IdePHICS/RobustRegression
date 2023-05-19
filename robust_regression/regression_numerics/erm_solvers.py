66b2e25c343cd84e585fc07aa4faa8344871e2bd
Matteo Vilucchio <35531309+superporchetta@users.noreply.github.com>
2023-05-20 00:23:21 +0200

    Unjitted loss_gradient_huber

--- robust_regression/regression_numerics/erm_solvers.py
+++ robust_regression/regression_numerics/erm_solvers.py
@@ -14,9 +14,12 @@ from numpy import (
     empty,
     sum,
 )
+import numpy as np
 import numpy.linalg as LA
 from numpy.random import normal
 from numba import njit
+from sklearn.utils import axis0_safe_slice
+from sklearn.utils.extmath import safe_sparse_dot
 from scipy.optimize import minimize, line_search
 from cvxpy import Variable, Minimize, Problem, norm, sum_squares
 from ..utils.matrix_utils import axis0_pos_neg_mask, safe_sparse_dot
@@ -42,30 +45,60 @@ def find_coefficients_L1(ys, xs, reg_param):
     return w.value
 
 
-@njit(error_model="numpy")
+# @njit(error_model="numpy")
+# def _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a):
+#     linear_loss = ys - xs_norm @ w
+#     abs_linear_loss = abs(linear_loss)
+#     outliers_mask = abs_linear_loss > a
+
+#     outliers = abs_linear_loss[outliers_mask]
+#     num_outliers = count_nonzero(outliers_mask)
+#     n_non_outliers = xs_norm.shape[0] - num_outliers
+
+#     loss = a * sum(outliers) - 0.5 * num_outliers * a**2
+
+#     non_outliers = linear_loss[~outliers_mask]
+#     loss += 0.5 * dot(non_outliers, non_outliers)
+#     loss += 0.5 * reg_param * dot(w, w)
+
+#     (xs_outliers, xs_non_outliers) = axis0_pos_neg_mask(xs_norm, outliers_mask, num_outliers)
+#     xs_non_outliers *= -1.0
+
+#     gradient = safe_sparse_dot(non_outliers, xs_non_outliers)
+
+#     signed_outliers = ones_like(outliers)
+#     signed_outliers_mask = linear_loss[outliers_mask] < 0
+#     signed_outliers[signed_outliers_mask] = -1.0
+
+#     gradient -= a * safe_sparse_dot(signed_outliers, xs_outliers)
+#     gradient += reg_param * w
+
+#     return loss, gradient
+
+
 def _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a):
     linear_loss = ys - xs_norm @ w
-    abs_linear_loss = abs(linear_loss)
+    abs_linear_loss = np.abs(linear_loss)
     outliers_mask = abs_linear_loss > a
 
     outliers = abs_linear_loss[outliers_mask]
-    num_outliers = count_nonzero(outliers_mask)
+    num_outliers = np.count_nonzero(outliers_mask)
     n_non_outliers = xs_norm.shape[0] - num_outliers
 
-    loss = a * sum(outliers) - 0.5 * num_outliers * a**2
+    loss = a * np.sum(outliers) - 0.5 * num_outliers * a**2
 
     non_outliers = linear_loss[~outliers_mask]
-    loss += 0.5 * dot(non_outliers, non_outliers)
-    loss += 0.5 * reg_param * dot(w, w)
+    loss += 0.5 * np.dot(non_outliers, non_outliers)
+    loss += 0.5 * reg_param * np.dot(w, w)
 
-    (xs_outliers, xs_non_outliers) = axis0_pos_neg_mask(xs_norm, outliers_mask, num_outliers)
-    xs_non_outliers *= -1.0
-
+    xs_non_outliers = -axis0_safe_slice(xs_norm, ~outliers_mask, n_non_outliers)
     gradient = safe_sparse_dot(non_outliers, xs_non_outliers)
 
-    signed_outliers = ones_like(outliers)
+    signed_outliers = np.ones_like(outliers)
     signed_outliers_mask = linear_loss[outliers_mask] < 0
     signed_outliers[signed_outliers_mask] = -1.0
+
+    xs_outliers = axis0_safe_slice(xs_norm, outliers_mask, num_outliers)
 
     gradient -= a * safe_sparse_dot(signed_outliers, xs_outliers)
     gradient += reg_param * w
@@ -73,9 +106,9 @@ def _loss_and_gradient_Huber(w, xs_norm, ys, reg_param, a):
     return loss, gradient
 
 
-def find_coefficients_Huber(ys, xs, reg_param, a):
+def find_coefficients_Huber(ys, xs, reg_param, a, scale_init=1.0):
     _, d = xs.shape
-    w = normal(loc=0.0, scale=1.0, size=(d,))
+    w = normal(loc=0.0, scale=scale_init, size=(d,))
     xs_norm = divide(xs, sqrt(d))
 
     bounds = tile([-inf, inf], (w.shape[0], 1))
